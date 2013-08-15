89de04b627fec5a44ff2048d2ec8514f8527b338
Will Cosgrove <will@willcosgrove.com>
2013-08-15 17:58:45 -0500

    Clean up notifiable

--- lib/notably/notifiable.rb
+++ lib/notably/notifiable.rb
@@ -1,20 +1,20 @@
 module Notably
   module Notifiable
 
-    def unread_notifications
-      parse_notifications(Notably.config.redis.zrevrangebyscore(notification_key, Time.now.to_i, last_notification_read_at))
-    end
-
-    def unread_notifications!
-      parse_notifications(Notably.config.redis.zrevrangebyscore(notification_key, Time.now.to_i, Notably.config.redis.getset(last_notification_read_at_key, Time.now.to_i)))
-    end
-
     def notifications
       parse_notifications(Notably.config.redis.zrevrangebyscore(notification_key, Time.now.to_i, 0))
     end
 
     def notifications_since(time)
       parse_notifications(Notably.config.redis.zrevrangebyscore(notification_key, Time.now.to_i, time.to_i))
+    end
+
+    def unread_notifications
+      notifications_since(last_notification_read_at)
+    end
+
+    def unread_notifications!
+      notifications_since(Notably.config.redis.getset(last_notification_read_at_key, Time.now.to_i))
     end
 
     def read_notifications
@@ -26,18 +26,8 @@ module Notably
     end
 
     def last_notification_read_at
-      Notably.config.redis.get(last_notification_read_at_key).to_i || 0
+      Notably.config.redis.get(last_notification_read_at_key).to_i
     end
-
-    def push_notification(notification, time)
-      Notably.config.redis.zadd(notification_key, time, notification)
-    end
-
-    def delete_notification(notification)
-      Notably.config.redis.zrem(notification_key, notification)
-    end
-
-    private
 
     def notification_key
       "notably:notifications:#{self.class}:#{self.id}"
@@ -46,6 +36,8 @@ module Notably
     def last_notification_read_at_key
       "notably:last_read_at:#{self.class}:#{self.id}"
     end
+
+    private
 
     def parse_notifications(notifications)
       notifications.collect { |n| Marshal.load(n) }
